f99c86ae18114bb21a7e5fdbb863bdf2da441d34
Libby Duggan <libby.duggan@gmail.com>
2023-07-03 20:41:23 -0400

    added delete route

--- books.py
+++ books.py
@@ -55,3 +55,29 @@ async def create_book(new_book=Body()):
 
 
 
+@app.put("/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+
+@app.delete("/delete_book/{book_title}")
+async def delete_book_by_title(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+
+
+@app.get("/books/byauthor/{author}")
+async def get_books_by_auther(author: str):
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
